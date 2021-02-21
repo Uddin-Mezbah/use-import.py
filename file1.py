d91a709957efb94bf300227d4774c93a2fc66ee1
Mezbah uddin(davide) <75944104+davide666777@users.noreply.github.com>
2021-02-21 21:27:59 +0800

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,9 @@
+from sklearn.ensemble import RandomForestClassifier
+print(RandomForestClassifier())
+
+import file2
+print(file2.a)
+file2.printworld("This is me")
+
+#note = don't create folder any module name
+   #"be careful"
